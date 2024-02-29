108b70f47cf8d236f68c160e70d184af991b8429
KMH <seaweed8759@naver.com>
2024-02-29 18:46:28 +0900

    Add : git init

---
+++ src/main/java/com/kobaco/kobaco_project/KobacoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kobaco.kobaco_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KobacoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KobacoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kobaco/kobaco_project/KobacoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kobaco.kobaco_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KobacoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
