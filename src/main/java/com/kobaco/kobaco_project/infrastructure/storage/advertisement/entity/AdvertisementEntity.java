66d64313b2e6b98f89b627e14fae492f08a8a2ae
seeunjang <wisjang1@naver.com>
2024-03-01 16:06:17 +0900

    feat : 광고 기본 정보 조희 API - Infrastructure 계층 생성

---
+++ src/main/java/com/kobaco/kobaco_project/infrastructure/storage/advertisement/entity/AdvertisementEntity.java
@@ -0,0 +1,38 @@
+package com.kobaco.kobaco_project.infrastructure.storage.advertisement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "advertisement")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AdvertisementEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String videoUrl;
+    private String advertiser;
+    private String agency;
+    private String manufacturer;
+    private Boolean isArchived = Boolean.FALSE;
+
+    private AdvertisementEntity(String title, String videoUrl, String advertiser, String agency, String manufacturer) {
+        this.title = title;
+        this.videoUrl = videoUrl;
+        this.advertiser = advertiser;
+        this.agency = agency;
+        this.manufacturer = manufacturer;
+    }
+
+    public static AdvertisementEntity of(String title, String videoUrl, String advertiser, String agency, String manufacturer) {
+        return new AdvertisementEntity(title, videoUrl, advertiser, agency, manufacturer);
+    }
+}
