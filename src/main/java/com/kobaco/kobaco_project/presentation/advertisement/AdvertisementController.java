6dfecfe3b9678fab2673a3d6e98d3ee385601492
seeunjang <wisjang1@naver.com>
2024-03-01 16:03:47 +0900

    feat : 광고 기본 정보 조희 API - Presentation 계층 생성

---
+++ src/main/java/com/kobaco/kobaco_project/presentation/advertisement/AdvertisementController.java
@@ -0,0 +1,25 @@
+package com.kobaco.kobaco_project.presentation.advertisement;
+
+import com.kobaco.kobaco_project.application.advertisement.AdvertisementApplication;
+import com.kobaco.kobaco_project.application.advertisement.dto.response.AdvertisementInfoResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/advertisement")
+@RequiredArgsConstructor
+public class AdvertisementController {
+
+    private final AdvertisementApplication advertisementApplication;
+
+    @GetMapping("/info/{advertisementId}")
+    public AdvertisementInfoResponse getAdvertisementInfo(@PathVariable Long advertisementId) {
+        AdvertisementInfoResponse advertisementInfo = advertisementApplication.getAdvertisementInfo(advertisementId);
+        return advertisementInfo;
+    }
+}
