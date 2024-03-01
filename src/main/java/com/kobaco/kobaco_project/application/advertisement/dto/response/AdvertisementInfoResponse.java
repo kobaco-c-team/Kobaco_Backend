1b10c5890aa0acdd582f5dc518dd567be7b0a7d8
seeunjang <wisjang1@naver.com>
2024-03-01 16:33:01 +0900

    refactor : 응답값에 스크랩여부 값 추가

--- src/main/java/com/kobaco/kobaco_project/application/advertisement/dto/response/AdvertisementInfoResponse.java
+++ src/main/java/com/kobaco/kobaco_project/application/advertisement/dto/response/AdvertisementInfoResponse.java
@@ -2,10 +2,10 @@ package com.kobaco.kobaco_project.application.advertisement.dto.response;
 
 import com.kobaco.kobaco_project.domain.advertisement.Advertisement;
 
-public record AdvertisementInfoResponse(String videoUrl, String advertiser, String agency, String manufacturer) {
+public record AdvertisementInfoResponse(String videoUrl, String advertiser, String agency, String manufacturer, boolean isArchived) {
 
     public static AdvertisementInfoResponse from(final Advertisement advertisement) {
         return new AdvertisementInfoResponse(advertisement.getVideoUrl(), advertisement.getAdvertiser(),
-                advertisement.getAgency(), advertisement.getManufacturer());
+                advertisement.getAgency(), advertisement.getManufacturer(), advertisement.getIsArchived());
     }
 }
