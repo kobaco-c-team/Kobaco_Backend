f9283c5e4cce98a2525ea1102ba4b90af99fa102
seeunjang <wisjang1@naver.com>
2024-03-01 16:07:21 +0900

    refactor : Entity 클래스 BaseEntity extends 하도록 수정

--- src/main/java/com/kobaco/kobaco_project/infrastructure/storage/advertisement/entity/AdvertisementEntity.java
+++ src/main/java/com/kobaco/kobaco_project/infrastructure/storage/advertisement/entity/AdvertisementEntity.java
@@ -1,5 +1,6 @@
 package com.kobaco.kobaco_project.infrastructure.storage.advertisement.entity;
 
+import com.kobaco.kobaco_project.infrastructure.storage.common.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "advertisement")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class AdvertisementEntity {
+public class AdvertisementEntity extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
