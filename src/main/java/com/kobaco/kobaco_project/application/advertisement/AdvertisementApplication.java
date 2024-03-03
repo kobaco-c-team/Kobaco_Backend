90d8938366687ad455095ebb8fc82731e007ddc2
KMH <seaweed8759@naver.com>
2024-03-04 00:29:57 +0900

    update : 영상 분석 결과 조회 api - Application 계층에서 category별 함수 분기 처리

--- src/main/java/com/kobaco/kobaco_project/application/advertisement/AdvertisementApplication.java
+++ src/main/java/com/kobaco/kobaco_project/application/advertisement/AdvertisementApplication.java
@@ -12,7 +12,6 @@ import com.kobaco.kobaco_project.domain.advertisement.service.ReadPerson;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @ApplicationService
 @RequiredArgsConstructor
 public class AdvertisementApplication {
@@ -22,6 +21,7 @@ public class AdvertisementApplication {
     private final ReadMood readMood;
     private final ReadItem readItem;
     private final ReadPerson readPerson;
+    private final ReadPlace readPlace;
     private final ReadAdvertisementSimilar readAdvertisementSimilar;
     private final ArchiveAdvertisement archiveAdvertisement;
     private final ReadAiAnalysis readAiAnalysis;
@@ -79,7 +79,35 @@ public class AdvertisementApplication {
         archiveAdvertisement.archiveAdvertisement(advertisementId);
     }
 
-    public AdvertisementAiAnalysisResponse<Object> getAiAnalysis(Long advertisementId, String category) {
-        return new AdvertisementAiAnalysisResponse<>(category, readAiAnalysis.readAiAnalysis(advertisementId, category));
+    public AdvertisementAiAnalysisResponse getAiAnalysis(Long advertisementId, String category) {
+        if (category.equals("person")) {
+            return AdvertisementAiAnalysisResponse.of(category,
+                    readPerson.getPerson(advertisementId)
+                            .stream()
+                            .map(person -> PersonInfoResponse.of(person.getName()))
+                            .toList(),
+                    null,null,
+                    readAiAnalysis.readAiAnalysis(advertisementId, category));
+        }
+        if(category.equals("object")){
+            return AdvertisementAiAnalysisResponse.of(category,
+                    null,
+                    readItem.getItem(advertisementId)
+                            .stream()
+                            .map(item -> ItemInfoResponse.of(item.getName()))
+                            .toList(),
+                    null,
+                    readAiAnalysis.readAiAnalysis(advertisementId, category));
+        }
+        if(category.equals("place")){
+            return AdvertisementAiAnalysisResponse.of(category,
+                    null,null,
+                    readPlace.getPlaee(advertisementId)
+                            .stream()
+                            .map(place -> PlaceInfoResponse.of(place.getName()))
+                            .toList(),
+                    readAiAnalysis.readAiAnalysis(advertisementId, category));;
+        }
+        return null;
     }
 }
---
+++ src/main/java/com/kobaco/kobaco_project/domain/advertisement/service/ReadAiAnalysis.java
@@ -0,0 +1,15 @@
+package com.kobaco.kobaco_project.domain.advertisement.service;
+
+import com.kobaco.kobaco_project.common.annotation.DomainService;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@DomainService
+@RequiredArgsConstructor
+public class ReadAiAnalysis {
+
+    public List<String> readAiAnalysis(Long advertisementId, String category) {
+
+    }
+}
