e118d5cc886e5218661c49db577ca9aad9297f4f
seeunjang <wisjang1@naver.com>
2024-03-03 18:25:08 +0900

    feat : import문 추가

--- src/main/java/com/kobaco/kobaco_project/presentation/advertisement/AdvertisementController.java
+++ src/main/java/com/kobaco/kobaco_project/presentation/advertisement/AdvertisementController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
