11d613c0c28c62898541223085ffbcdbcbff3df5
seeunjang <wisjang1@naver.com>
2024-03-03 18:24:23 +0900

    feat : API 설명 추가

--- src/main/java/com/kobaco/kobaco_project/presentation/advertisement/AdvertisementController.java
+++ src/main/java/com/kobaco/kobaco_project/presentation/advertisement/AdvertisementController.java
@@ -3,6 +3,7 @@ package com.kobaco.kobaco_project.presentation.advertisement;
 import com.kobaco.kobaco_project.application.advertisement.AdvertisementApplication;
 import com.kobaco.kobaco_project.application.advertisement.dto.response.AdvertisementAnalysisResponse;
 import com.kobaco.kobaco_project.application.advertisement.dto.response.AdvertisementInfoResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,12 +19,14 @@ public class AdvertisementController {
 
     private final AdvertisementApplication advertisementApplication;
 
+    @Operation(summary = "광고 기본 정보 조회")
     @GetMapping("/info/{advertisementId}")
     public AdvertisementInfoResponse getAdvertisementInfo(@PathVariable Long advertisementId) {
         AdvertisementInfoResponse advertisementInfo = advertisementApplication.getAdvertisementInfo(advertisementId);
         return advertisementInfo;
     }
 
+    @Operation(summary = "광고 영상 분석 결과 조회(영상 분위기, 인물, 사물)")
     @GetMapping("/analysis/{advertisementId}")
     public AdvertisementAnalysisResponse getAdvertisementAnalysis(@PathVariable Long advertisementId) {
         AdvertisementAnalysisResponse advertisementAnalysis = advertisementApplication.getAdvertisementAnalysis(advertisementId);
