5d77f83dbfcef103c5303e3efba0d201738c4924
seeunjang <wisjang1@naver.com>
2024-03-06 03:43:33 +0900

    feat : 트렌드 월별 & 요일별 분석 조회 API 생성

--- src/main/java/com/kobaco/kobaco_project/presentation/trend/TrendController.java
+++ src/main/java/com/kobaco/kobaco_project/presentation/trend/TrendController.java
@@ -1,7 +1,8 @@
 package com.kobaco.kobaco_project.presentation.trend;
 
 import com.kobaco.kobaco_project.application.trend.TrendApplication;
-import com.kobaco.kobaco_project.application.trend.dto.response.PersonTrendResponse;
+import com.kobaco.kobaco_project.application.trend.dto.response.TimeStatisticResponse;
+import com.kobaco.kobaco_project.application.trend.dto.response.PersonStatisticResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +23,12 @@ public class TrendController {
     @GetMapping("/person")
     public PersonStatisticResponse getPersonStatistic(@RequestParam String trendKwd) {
         return trendApplication.getPersonStatistic(trendKwd);
+    }
+
+    @Operation(summary = "트렌드 월별 & 요일별 분석 조회")
+    @GetMapping("/time")
+    public TimeStatisticResponse getTimeStatistic(@RequestParam String trendKwd) {
+        return trendApplication.getTimeStatistic(trendKwd);
     }
 
 
