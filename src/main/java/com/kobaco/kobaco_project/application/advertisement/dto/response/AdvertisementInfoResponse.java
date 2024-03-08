bb29eec4a6f4978e2e773abee1ff0ff176432e4f
seeunjang <wisjang1@naver.com>
2024-03-09 00:49:02 +0900

    refactor : 광고 기본 정보 조회 API 응답값 수정

--- src/main/java/com/kobaco/kobaco_project/application/advertisement/dto/response/AdvertisementInfoResponse.java
+++ src/main/java/com/kobaco/kobaco_project/application/advertisement/dto/response/AdvertisementInfoResponse.java
@@ -1,11 +1,12 @@
 package com.kobaco.kobaco_project.application.advertisement.dto.response;
 
 import com.kobaco.kobaco_project.domain.advertisement.model.Advertisement;
+import java.time.LocalDate;
 
-public record AdvertisementInfoResponse(String videoUrl, String advertiser, String agency, String manufacturer, boolean isArchived) {
+public record AdvertisementInfoResponse(String videoUrl, String title, LocalDate createdAt, String advertiser, String agency, String manufacturer, boolean isArchived) {
 
     public static AdvertisementInfoResponse from(final Advertisement advertisement) {
-        return new AdvertisementInfoResponse(advertisement.getVideoUrl(), advertisement.getAdvertiser(),
+        return new AdvertisementInfoResponse(advertisement.getVideoUrl(), advertisement.getTitle(), advertisement.getCreatedAt().toLocalDate(), advertisement.getAdvertiser(),
                 advertisement.getAgency(), advertisement.getManufacturer(), advertisement.getIsArchived());
     }
 }
